ce10f27d076a5532ce160f9482ee6592a11e9f28
color.ui <timur67@list.ru>
2022-02-15 02:40:43 +0200

    refactoring code filecreate5

--- filecreate5/main.go
+++ filecreate5/main.go
@@ -23,41 +23,46 @@ func CreateFile(i int) (retErr error) {
 
 	for i := 0; i < 5; i++ {
 
-		fileName := strconv.Itoa(i) + ".txt"
-		//error provocation by func os.Create()
-		//fileName = string(i) + ".txt"
+		retErr = func() (retErrIn error) {
+			fileName := strconv.Itoa(i) + ".txt"
 
-		f, err := os.Create(fileName)
-		if err != nil {
-			err = errors.Wrapf(err, "error in func CreateFile() os.Create()")
-			return err // sets retErr = err
-		}
-		fmt.Printf("file %s Created\n", fileName)
+			//todo error provocation by func os.Create()
+			//fileName = string(i) + ".txt"
 
-		//error provocation by func f.Close()()
-		//defer closeFile(f, fileName)
-		message := "message Nr: " + strconv.Itoa(i)
-		_, err = fmt.Fprintln(f, message)
-		if err != nil {
-			err = errors.Wrapf(err, "error in func CreateFile() fmt.Fprintln()")
-			return err // sets retErr = err
-		}
-		fmt.Printf("file %s Printed in file\n", fileName)
+			f, err := os.Create(fileName)
 
-		// вообще-то это то же самое, что прсто closeFile(f, fileName)
-		//т.е. вызов closeFile идет в этом же самом месте после выхода из анонимной функции
-		//тогда именно в такой ситуации в чем смысл defer?
-		//а вот ошибку из-под defer никак не вытащить.
-		func() { defer closeFile(f, fileName) }()
+			if err != nil {
+				err = errors.Wrapf(err, "error in func CreateFile() os.Create()")
+				return err // sets retErrIn = err
+			}
+
+			defer closeFile(f, fileName)
+
+			fmt.Printf("file %s Created\n", fileName)
+
+			//todo error provocation by func f.Close()
+			//defer closeFile(f, fileName)
+
+			message := "message Nr: " + strconv.Itoa(i)
+
+			_, err = fmt.Fprintln(f, message)
+			if err != nil {
+				err = errors.Wrapf(err, "error in func CreateFile() fmt.Fprintln()")
+				return err // sets retErrIn = err
+			}
+
+			fmt.Printf("file %s Printed in file\n", fileName)
+
+			return nil
+		}()
 	}
-	return nil
+	return //retErr
 }
 
 func closeFile(f *os.File, fileName string) {
 	if err := f.Close(); err != nil {
 		err = errors.Wrapf(err, "error in func closeFile() f.Close()")
 		fmt.Println(err)
-		os.Exit(1)
 	} else {
 		fmt.Printf("file %v Closed\n", fileName)
 	}
