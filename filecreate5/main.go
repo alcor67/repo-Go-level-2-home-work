7127f6c11a206c76d26a4141483e5383b2bb36fd
color.ui <timur67@list.ru>
2022-02-15 02:56:27 +0200

    refactoring code

--- filecreate5/main.go
+++ filecreate5/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	err := CreateFile(5)
+	err := createFile(5)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -19,7 +19,7 @@ func main() {
 
 }
 
-func CreateFile(i int) (retErr error) {
+func createFile(i int) (retErr error) {
 
 	for i := 0; i < 5; i++ {
 
